98d9d531ba239ecfdbf3af2fb98e06c3cabc7835
Vincent Dao <vincentdao288@gmail.com>
2026-09-14 19:45:59 +1000

    feat: add frontend navbar component, no functionality

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+export default function Navbar(){
+  return(
+    <div className="absolute w-full flex justify-between p-4 bg-black text-white">
+      <div className="cursor-pointer hover:scale-105 transition-transform duration-200">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-8 w-8"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </div>
+      <a 
+        className="cursor-pointer hover:scale-105 transition-transform duration-200"
+        href="/login">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-7 w-7"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+        </svg>
+      </a>
+    </div>
+  )
+}
