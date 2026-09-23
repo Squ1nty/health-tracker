80e0e80aea694f30f967017a33af390cd99926d7
Vincent Dao <vincentdao288@gmail.com>
2026-09-23 11:45:11 +1000

    feat: add herosection wayfinding so user can login/signup

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,8 +1,14 @@
 export default function HeroSection(){
   return(
-    <div className="w-full flex flex-col items-center gap-2">
-      <h1 className="leading-1 text-3xl font-bold text-center">Welcome to <span className='text-nowrap text-5xl'><br />Health Tracker!</span></h1>
-      <p className="text-center text-gray-600">Track your health and wellness goals with ease.</p>
+    <div className="w-full min-h-svh grid place-items-center">
+      <div className="w-full flex flex-col items-center gap-2">
+        <h1 className="text-3xl font-bold text-center">Welcome to <span className='text-nowrap text-5xl'><br />Health Tracker!</span></h1>
+        <p className="text-center text-gray-600">Track your health and wellness goals with ease.</p>
+      </div>
+      <div className="mt-10 w-full flex flex-col items-center gap-2">
+        <p>Login or Sign up to get started!</p>
+      </div>
     </div>
+
   );
 }
